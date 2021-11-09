b123e01812cd77162571b8a61a2644f912ea46f1
xiezhang <xiezhang@redhat.com>
2021-11-08 21:59:55 -0500

    create new CRD for provisioning

--- main.go
+++ main.go
@@ -172,6 +172,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DBaaSPlatform")
 		os.Exit(1)
 	}
+	if err = (&controllers.DBaaSInstanceReconciler{
+		DBaaSReconciler: DBaaSReconciler,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DBaaSInstance")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
